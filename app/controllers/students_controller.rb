17a3043dd0a63ceb5190239585b444d701b9721a
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-04-07 19:17:21 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
+require "pry"
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -12,6 +13,12 @@ class StudentsController < ApplicationController
   end
 
   def create
+    if params[:first_name] && params[:last_name]
+      @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    else
+      @student = Student.create(params[:student].permit!)
+    end
+    redirect_to student_path(@student)
   end
 
 end
